b7b67c7b908cb67f0811eba6d81311543b4aefac
Donato Marrazzo <donato.marrazzo@gmail.com>
2021-06-15 20:34:26 +0200

    init

---
+++ src/main/java/com/redhat/example/KieServerExtensionDeploy.java
@@ -0,0 +1,116 @@
+package com.redhat.example;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.ServiceLoader;
+
+import org.jbpm.kie.services.impl.KModuleDeploymentService;
+import org.kie.server.services.api.KieContainerInstance;
+import org.kie.server.services.api.KieServerApplicationComponentsService;
+import org.kie.server.services.api.KieServerExtension;
+import org.kie.server.services.api.KieServerRegistry;
+import org.kie.server.services.api.SupportedTransports;
+import org.kie.server.services.impl.KieServerImpl;
+import org.kie.server.services.jbpm.JbpmKieServerExtension;
+
+public class KieServerExtensionDeploy implements KieServerExtension {
+
+    private boolean initialized = false;
+    private static final Boolean disabled = false;
+    private static final String EXTENSION_NAME = "DeploymentListener";
+    private KieServerRegistry context;
+
+    @Override
+    public boolean isInitialized() {
+        return initialized;
+    }
+
+    @Override
+    public boolean isActive() {
+        return !disabled;
+    }
+
+    @Override
+    public void init(KieServerImpl kieServer, KieServerRegistry registry) {
+
+        // Prometheus Monitoring
+        KieServerExtension jBPMExtension = registry.getServerExtension(JbpmKieServerExtension.EXTENSION_NAME);
+        if (jBPMExtension != null) {
+
+            final KModuleDeploymentService deploymentService = jBPMExtension
+                    .getAppComponents(KModuleDeploymentService.class);
+            if (deploymentService != null) {
+                deploymentService.addListener(new TestDeploymentListener());
+            }
+        }
+        context = registry;
+        initialized = true;
+    }
+
+    @Override
+    public void destroy(KieServerImpl kieServer, KieServerRegistry registry) {
+    }
+
+    @Override
+    public void createContainer(String id, KieContainerInstance kieContainerInstance, Map<String, Object> parameters) {
+    }
+
+    @Override
+    public void updateContainer(String id, KieContainerInstance kieContainerInstance, Map<String, Object> parameters) {
+    }
+
+    @Override
+    public boolean isUpdateContainerAllowed(String id, KieContainerInstance kieContainerInstance,
+            Map<String, Object> parameters) {
+        return true;
+    }
+
+    @Override
+    public void disposeContainer(String id, KieContainerInstance kieContainerInstance, Map<String, Object> parameters) {
+    }
+
+    @Override
+    public List<Object> getAppComponents(SupportedTransports type) {
+    	
+        ServiceLoader<KieServerApplicationComponentsService> appComponentsServices
+                = ServiceLoader.load( KieServerApplicationComponentsService.class );
+        List<Object> appComponentsList = new ArrayList<Object>();
+        Object[] services = {context};
+        for ( KieServerApplicationComponentsService appComponentsService : appComponentsServices ) {
+            appComponentsList.addAll( appComponentsService.getAppComponents( EXTENSION_NAME, type, services ) );
+        }
+        return appComponentsList;
+    }
+
+    @Override
+    public <T> T getAppComponents(Class<T> serviceType) {
+        return null;
+    }
+
+    @Override
+    public String getImplementedCapability() {
+        return "DeploymentListener";
+    }
+
+    @Override
+    public List<Object> getServices() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public String getExtensionName() {
+        return EXTENSION_NAME;
+    }
+
+    @Override
+    public Integer getStartOrder() {
+        return Integer.MAX_VALUE;
+    }
+
+    @Override
+    public String toString() {
+        return EXTENSION_NAME + " KIE Server extension";
+    }
+}
---
+++ src/main/java/com/redhat/example/TestDeploymentListener.java
@@ -0,0 +1,31 @@
+package com.redhat.example;
+
+import org.jbpm.services.api.DeploymentEvent;
+import org.jbpm.services.api.DeploymentEventListener;
+
+public class TestDeploymentListener implements DeploymentEventListener {
+
+    @Override
+    public void onDeploy(DeploymentEvent event) {
+        onEvent(event);
+    }
+
+    @Override
+    public void onUnDeploy(DeploymentEvent event) {
+        onEvent(event);
+    }
+
+    @Override
+    public void onActivate(DeploymentEvent event) {
+        onEvent(event);
+    }
+
+    @Override
+    public void onDeactivate(DeploymentEvent event) {
+        onEvent(event);
+    }
+
+    private void onEvent(DeploymentEvent event) {
+        System.out.println("TestDeploymentListener.onEvent() " + event);
+    }
+}
